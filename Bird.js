f62fbb4fa0afe063b4d1be309c2786f7771ca5fa
Sayan Ghosh <sgic2001@gmail.com>
2020-11-25 10:39:02 +0530

    Update to add colors and remove text

--- Bird.js
+++ Bird.js
@@ -1,46 +1,38 @@
 function Bird()
 {
-	this.x= width/2;
-	this.y=height/2;
+	this.x = width/2;
+	this.y = height/2;
 	this.vel = 0;
 	this.gravity = 0.8;
 	this.lift = -15;
 	this.multiplier=0.9;
 	this.r = 10;
-
 	this.show = function()
 	{
-		var degree = map (this.vel,-15,15,-PI/2,PI/2);
+		var degree = map(this.vel,-15,15,-PI/2,PI/2);
 		//beak
 		push();
 		translate(this.x,this.y);
 		rotate(degree);
 		fill(255);
 		noStroke();
-		// fill(255,99,71);
+		 fill(255,99,71);
 		triangle (7,-5,7,5,15,-3);
 		
 		//body
-		// fill(225,225,20);
+		 fill(225,225,20);
 		ellipse(0,0,this.r*2,this.r*2);
 		//eyes
-		// fill(255);
-		// ellipse(3,-3,4,4);
-		// fill(0);
-		// ellipse(3,-3,2,2);
-
-
-
+		 fill(255);
+		 ellipse(3,-3,4,4);
+		 fill(0);
+		 ellipse(3,-3,2,2);
 		pop();
-
-
 	};
-
 	this.update = function(){
 		this.vel += this. gravity;
 		this.vel *= this.multiplier;
 		this.y += this.vel;
-
 		if (this.y>height){
 			this.y = height;
 			gameOver();
@@ -49,13 +41,14 @@ function Bird()
 			this.y = 0;
 			gameOver();
 		}
-
-
 	};
-	
 	this.jump = function(){
+		if(!gameOverbool){
+			loop();
 			this.vel += this.lift;
-			// console.log("Jumping");
-
+		}
+		else {
+			location.reload();
+		}
 	};
 }
--- Main.js
+++ Main.js
@@ -2,32 +2,23 @@ var Bird;
 var pipes = [];
 var gameOverbool = false;
 var score = 0;
-
-
-
 function setup() {
 	createCanvas(225,400);
 	background(50,50,100);
-	createP("Flappy Bird     by Sayan");
 	bird = new Bird();
 	pipes[0] = new Pipe();
 	noLoop();
 }
-
-
-
 var count = 0;
 var interval = 100;
-var string ;
-
-
+var string;
 function draw() {
-	if((frameCount%interval === 0)&&!gameOverbool)
+	if(frameCount % interval === 0 && !gameOverbool)
 	{
 		pipes.push(new Pipe());
+		// increase difficulty for every new pipe
 		bird.gravity+=0.03;
 		bird.lift-=0.3;
-
 		if(Pipe.gapmax > 50){
 			Pipe.gapmax--;
 		}
@@ -37,13 +28,10 @@ function draw() {
 		if(bird.multiplier<0.97){
 			bird.multiplier+=0.002;
 		}
-
 	}
 	background(0);
 	bird.show();
 	bird.update();
-
-
 	//pipes
 	for (var i = 0; i < pipes.length; i++) {
 		var pipe = pipes[i];
@@ -52,7 +40,6 @@ function draw() {
 		if(pipe.passed(bird)){
 			score++;
 		}
-
 		if(pipe.x<0){
 			pipes.splice(i,1);
 			i--;
@@ -61,45 +48,26 @@ function draw() {
 			gameOver();
 		}
 	}
-
-
-
-
 	string = "Score : "+ score;
 	len = string.length;
 	fill(255);
 	stroke(0);
 	strokeWeight(1);
 	textSize(16);
-	text(string , width - (8*len) , 16);
-
-
+	text(string, width - (8*len), 16);
 }
-
 function keyPressed(){
-	if(!gameOverbool){
-		loop();
-	}
-	if(key === 'R'){
-		location.reload();
-	}
-	 bird.jump();
-
+	bird.jump();
 }
 function touchStarted(){
-	if(!gameOverbool){
-		loop();
-	}
 	bird.jump();
 }
 function gameOver(){
-		gameOverbool = true;
-		bird.vel = 0;
-		bird.gravity=0;
-		bird.lift =0;
-		for (var i = 0; i < pipes.length; i++) {
-			pipes[i].speed = 0;
-		}
-		createP("Game Over!");
-		createP("Score : "+score);
+	gameOverbool = true;
+	bird.vel = 0;
+	bird.gravity = 0;
+	bird.lift = 0;
+	for (var i = 0; i < pipes.length; i++) {
+		pipes[i].speed = 0;
 	}
+}
